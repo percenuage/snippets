0f1ae9c11c534f6db0bf3b82acdeae718d588226
Axel Gendillard <percenuage@users.noreply.github.com>
2018-06-09 15:37:35 +0200

    Update searchMysteryNumber.java

--- searchMysteryNumber.java
+++ searchMysteryNumber.java
@@ -6,25 +6,38 @@ import java.util.stream.IntStream;
 
 public class Main {
 
+    private static final int MIN = 1;
     private static final int MAX = 100;
     private static final int FACTOR = 2;
     private static final int SECRET = 42;
-    private static final int LOOP = 100000;
+    private static final int LOOP = 1000;
 
     public static void main(String[] args) {
-        int[][] count = new int[2][LOOP];
-        for (int i = 0; i < LOOP; i++) {
-            count[0][i] = start(false);
-            count[1][i] = start(true);
-            System.out.println(i);
-        }
-        double[] computes = compute(count[0]);
+        int[][] results = loop(false);
+
+        double[] computes = compute(results[0]);
         System.out.println("Average by Dichotomy: " + computes[0]);
         System.out.println("Standard Deviation by Dichotomy: " + computes[1]);
 
-        computes = compute(count[1]);
+        computes = compute(results[1]);
         System.out.println("Average by Random: " + computes[0]);
         System.out.println("Standard Deviation by Random: " + computes[1]);
+    }
+
+    private static int[][] loop(boolean fixedSecret) {
+        int min = fixedSecret ? SECRET : MIN;
+        int max = fixedSecret ? SECRET: MAX - MIN + 1;
+        int[][] results = fixedSecret ? new int[2][LOOP] : new int[2][LOOP * max];
+        int index = 0;
+        for (int secret = min; secret <= max; secret++) {
+            for (int i = 0; i < LOOP; i++) {
+                results[0][index] = searchByDichotomy(secret);
+                results[1][index] = searchByRandom(secret);
+                index++;
+//                System.out.println(i + " • " + secret);
+            }
+        }
+        return results;
     }
 
     private static double[] compute(int... numbers) {
@@ -34,44 +47,40 @@ public class Main {
         return new double[] {mean, deviation};
     }
 
-    public static int start(boolean random) {
-        return random ? startByRand() : startByDichotomy();
-    }
-
-    private static int startByDichotomy() {
+    private static int searchByDichotomy(Integer secret) {
+        Integer min = MIN;
         Integer max = MAX;
         Integer number = -1;
         int count = 0;
-        Random rand = new Random();
-        while (!number.equals(SECRET)) {
+        while (!number.equals(secret)) {
             if (number >= 0) {
-                int compare = Integer.valueOf(SECRET).compareTo(number);
+                int compare = secret.compareTo(number);
                 if (compare > 0) {
-                    number = (max - number) / FACTOR + number;
+                    number = Math.max((max - number) / FACTOR, 1) + number;
                 } else {
                     max = number;
                     number /= FACTOR;
                 }
             } else {
-                number = rand.nextInt(MAX + 1);
+                number = (max - min) / FACTOR + min; // rand.nextInt(MAX + 1)
             }
             count++;
-            //System.out.println("Tentative #" + count + " • " + number);
+//            System.out.println("Tentative #" + count + " • " + number);
         }
-        //System.out.println("Found!");
+//        System.out.println("Found!");
         return count;
     }
 
-    private static int startByRand() {
-        Integer min = 0;
+    private static int searchByRandom(Integer secret) {
+        Integer min = MIN;
         Integer max = MAX;
         Integer number = -1;
-        int count = 0;
-        ArrayList<Integer> numbers = new ArrayList<>();
         Random rand = new Random();
-        while (!number.equals(SECRET)) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int count = 0;
+        while (!number.equals(secret)) {
             if (number >= 0) {
-                int compare = Integer.valueOf(SECRET).compareTo(number);
+                int compare = secret.compareTo(number);
                 if (compare > 0) {
                     min = number;
                 } else {
